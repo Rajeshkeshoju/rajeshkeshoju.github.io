2f41750698af18b5e090af99e4c673c23eceb1a9
Rajeshkeshoju <rajeshkeshoju9@gmail.com>
2020-11-23 12:09:16 +0530

    Modified backToTop.js

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,18 +1,17 @@
-<script>
-    topButton = document.getElementById("topBtn")
-    window.onscroll = function(){scrollFunction()}
 
-    function scrollFunction(){
-        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-            topButton.style.display="block"
-        }else{
-            topButton.style.display = "none"
-        }
+topButton = document.getElementById("topBtn")
+window.onscroll = function(){scrollFunction()}
+
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        topButton.style.display="block"
+    }else{
+        topButton.style.display = "none"
     }
+}
 
 
-    function backToTop(){
-        document.body.scrollTop =0;
-        document.documentElement.scrollTop = 0
-    }
-</script>
+function backToTop(){
+    document.body.scrollTop =0;
+    document.documentElement.scrollTop = 0
+}
