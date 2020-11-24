16a7f3774a5254142206f6c415cada4ff02f47bb
Rajeshkeshoju <rajeshkeshoju9@gmail.com>
2020-11-24 15:48:09 +0530

    responsive menu

--- js/responsive.js
+++ js/responsive.js
@@ -11,3 +11,12 @@ function backToTop(){
     document.body.scrollTop =0 //for Safari
     document.documentElement.scrollTop = 0  //for Chrome, Firefox , Edge
 }
+
+function expandMenu(){
+    var x = document.getElementById("topnav")
+    if(x.className == "header-nav"){
+        x.className += " responsive"
+    }else{
+        x.className = "header-nav"
+    }
+}
